0040cab4d4a1b343c9853127393e109d347f5c40
SheaMeyers <shea.meyers@gmail.com>
2015-10-31 17:36:18 -0600

    Created channels sidebar

--- app/app.js
+++ app/app.js
@@ -18,7 +18,17 @@ angular
     $stateProvider
       .state('home', {
         url: '/',
-        templateUrl: 'home/home.html'
+        templateUrl: 'home/home.html',
+
+        resolve:{
+          requireNoAuth: function($state, Auth){
+            return Auth.$requireAuth().then(function(auth){
+              $state.go('channels');
+            }, function(error){
+              return;
+            });
+          }
+        }
       })
       .state('login', {
         url: '/login',
@@ -67,6 +77,35 @@ angular
             });
           }
         }
+      })
+      .state('channels', {
+        url: '/channels',
+        controller: 'ChannelsCtrl as channelsCtrl',
+        templateUrl: 'channels/index.html',
+
+        resolve: {
+          channels: function(Channels){
+            return Channels.$loaded();
+          },
+          profile: function($state, Auth, Users){
+            return Auth.$requireAuth.then(function(auth){
+              return Users.getProfile(auth.uid).$loaded().then(function(profile){
+                if(profile.displayName){
+                  return profile;
+                } else {
+                  $state.go('profile');
+                }
+              });
+            }, function(error){
+              $state.go('home');
+            });
+          }
+        }
+      })
+      .state('channels.create', {
+        url: '/create',
+        templateUrl: 'channels/create.html',
+        controller: 'ChannelsCtrl as channelsCtrl'
       })
 
     $urlRouterProvider.otherwise('/');
